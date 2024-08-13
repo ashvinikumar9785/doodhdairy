6d0c3af39d51f328395b5efdd3e4b6db5cb44047
Ashvini Kumar <ashvini01210@gmail.com>
2024-08-13 22:58:27 +0530

    update api

--- src/app/utils/authentication.ts
+++ src/app/utils/authentication.ts
@@ -6,17 +6,17 @@ const jet_secret = process.env.JWT_SECRET;
 const verifyToken = (req: any, res: any, next: any) => {
     jwt.verify(req.headers["authorization"], jet_secret, async (err: any, decoded: { _id: any; iat: any; }) => {
         if (err || !decoded || !decoded._id) {
-        return sendUnauthorizedResponse({res});
+            return sendUnauthorizedResponse({ res });
 
 
         }
-        const user = await User.findOne({
+        const user: any = await User.findOne({
             _id: decoded._id,
             authTokenIssuedAt: decoded.iat * 1000, //enable when single login needed
         });
 
         if (!user) {
-        return sendUnauthorizedResponse({res});
+            return sendUnauthorizedResponse({ res });
 
 
         }
@@ -24,7 +24,7 @@ const verifyToken = (req: any, res: any, next: any) => {
         const currentDate = new Date().getTime();
         const interval = Math.floor((currentDate - loginDate) / (24 * 3600 * 1000)); // 1 day
         if (interval >= 1) {
-        return sendUnauthorizedResponse({res, message: 'SESSION_EXPIRE'});
+            return sendUnauthorizedResponse({ res, message: 'SESSION_EXPIRE' });
 
 
         }
